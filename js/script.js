65421ff0300955b1bffcf7b28a281c26a95934c8
mrcordova <46950394+mrcordova@users.noreply.github.com>
2024-07-08 20:10:53 -0700

    WIP search btn for lp logic

--- js/script.js
+++ js/script.js
@@ -1,7 +1,12 @@
+const searchBtn = document.querySelector(".search-btn");
+const input = document.getElementById("search");
 let map = L.map("map", {
   zoomControl: false,
   attributionControl: false,
 }).setView([51.505, -0.09], 13);
+
+const IP_API_KEY = "at_8ZsTEBLk0rIwuEVytt8BN4osGzwUQ";
+const geoIpify = "https://geo.ipify.org/api/v2/country,city?";
 
 let icon = L.icon({
   iconUrl: "../images/icon-location.svg",
@@ -18,3 +23,12 @@ L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
 }).addTo(map);
 
 L.marker([51.5, -0.09], { icon }).addTo(map);
+
+searchBtn.addEventListener("click", async (e) => {
+  const response = await fetch(
+    `${geoIpify}apiKey=${IP_API_KEY}&ipAddress=${input.value}`
+  );
+
+  const ipObj = await response.json();
+  console.info(ipObj);
+});
